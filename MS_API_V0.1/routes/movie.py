8852dd5a026af3393fd0ffb81de3d7d467cbe130
Javier Renero Balgañón <jreneb00@estudiantes.unileon.es>
2024-04-19 03:31:14 +0200

    Update movie.py routes to use consistent quotation marks and fix filter_movies endpoint

--- MS_API_V0.1/routes/movie.py
+++ MS_API_V0.1/routes/movie.py
@@ -12,12 +12,12 @@ from typing import List
 movie = APIRouter()
 
 
-@movie.get('/movies', response_model=list[Movie], tags=["Movies"])
+@movie.get("/movies", response_model=list[Movie], tags=["Movies"])
 async def find_all_movies():
     return moviesEntity(conn.find())
 
 
-@movie.post('/movies', response_model=Movie, tags=["Movies"])
+@movie.post("/movies", response_model=Movie, tags=["Movies"])
 async def create_movie(movie: Movie):
     new_movie = dict(movie)
     #del new_movie["id"]
@@ -26,7 +26,7 @@ async def create_movie(movie: Movie):
     return movieEntity(movie)
 
 
-@movie.get('/movies/{title}', response_model=Movie, tags=["Movies"])
+@movie.get("/movies/{title}", response_model=Movie, tags=["Movies"])
 async def find_movie(title: str):
     return movieEntity(conn.find_one({"title": title}))
 
@@ -40,10 +40,8 @@ async def update_movie(title: str, movie: Movie):
     })
     return movieEntity(conn.find_one({"title": title}))
 
-""" @movie.get('/movies/filter', response_model=list[Movie], tags=["Movies"])
-async def filter_movies(title: str = None, director: str = None, year: int = None, rating: int = None, genre: str = None):
-    print(title, director, year, rating, genre)
-    print("AQUISSSS")   
+@movie.get('/movies/filter/', response_model=list[Movie], tags=["Movies"])
+async def filter_movies(title: str = None, director: str = None, year: int = None, rating: int = None, genre: str = None):  
     query = {}
     if title is not None:
         query["title"] = title
@@ -55,10 +53,7 @@ async def filter_movies(title: str = None, director: str = None, year: int = Non
         query["rating"] = rating
     if genre is not None:
         query["genre"] = genre
-
-    return print("AQUISSSS"+name, director, year, rating, genre)
-   # return moviesEntity(conn.find({"title": name, "director": director, "year": year, "rating": rating, "genre": genre}))
- """
+    return moviesEntity(conn.find(query))
 
 @movie.delete("/movies/{title}", status_code=status.HTTP_204_NO_CONTENT, tags=["Movies"])
 async def delete_movie(title: str):
