784c0a77b1e143d81b7e27ae1f63ebc71a5061b1
Javier Renero Balgañón <jreneb00@estudiantes.unileon.es>
2024-04-20 03:44:05 +0200

    Update db.py with MongoDB connection details and usage example

--- MS_API_V0.1/config/db.py
+++ MS_API_V0.1/config/db.py
@@ -3,6 +3,16 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://admin:filmdump@filmdump-mongo-1")
 conn = client.filmdump.movies
 
+"""
+This module provides a connection to the MongoDB database for the FilmDump application.
 
+The `client` variable represents the MongoDB client object that connects to the database server.
+The `conn` variable represents the collection object for the 'movies' collection in the 'filmdump' database.
 
+Example usage:
+    # Connect to the MongoDB database
+    client = MongoClient("mongodb://admin:filmdump@filmdump-mongo-1")
 
+    # Access the 'movies' collection
+    conn = client.filmdump.movies
+"""
